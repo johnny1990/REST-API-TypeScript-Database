75f9a2bef6c5caa86cd0ae686288f81b9e5081a5
Man Ioan <man_ioan@live.com>
2020-10-04 16:43:45 +0300

    adjust

--- api_routes/shippersRoutes.ts
+++ api_routes/shippersRoutes.ts
@@ -1,6 +1,5 @@
 
     const _shippersRepository = require('./shippersRepository');
-    //const dbContext = require('../../database/dbContext');
     
     module.exports = function (router) {
         const categoryRepository = _categoryRepository(dbContext);router.route('api_routes/shippersRoutes')
